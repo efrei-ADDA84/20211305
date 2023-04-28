633982e15f544a1ccb28a6762d197f966b45d2a8
STang94 <stephane.tang@efrei.net>
2023-04-28 12:07:41 +0200

    TP2 [V0.8]

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os
 import requests
 from flask import Flask,request
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Homepage"
 
-@app.route('/weather', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_weather():
-    LAT = request.args.get('LAT')
-    LONG = request.args.get('LONG')
-    API_KEY = request.args.get('API_KEY')
+    LAT = request.args.get('lat')
+    LONG = request.args.get('lon')
+    API_KEY = os.getenv('API_KEY')
     
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={API_KEY}&units=metric'
     response = requests.get(url)
@@ -24,7 +24,7 @@ def get_weather():
         description = data['weather'][0]['description']
         return f"City : {city} \n Temperature : {temperature}°C \n Weather : {description}"
     else:
-        return "Problem of response"
+        return ("Problem of response", "response" ,response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8081)
