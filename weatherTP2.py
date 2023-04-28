96b92ba65e46a696cf756a7583d629c1dc58268d
STang94 <stephane.tang@efrei.net>
2023-04-28 14:06:44 +0200

    TP2 [V0.9]

--- weatherTP2.py
+++ weatherTP2.py
@@ -19,3 +19,38 @@ if response.status_code == 200:
 else:
     print(response)
     print("Problem of response")
+    
+
+import os
+import requests
+from flask import Flask,request
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_weather():
+    LAT = request.args.get('lat')
+    LONG = request.args.get('lon')
+    if LAT==None and LONG==None : 
+        LAT = "31.2504"
+        LONG = "-99.2506"
+    API_KEY = os.getenv('API_KEY')
+    
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LONG}&appid={API_KEY}&units=metric'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        city = data['name']
+        temperature = data['main']['temp']
+        description = data['weather'][0]['description']
+        return f"City : {city} \n Temperature : {temperature}°C \n Weather : {description}"
+    else:
+        return ("Problem of response", "response" ,response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
